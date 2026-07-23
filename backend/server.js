b1b887df706aa173d002b3487a024f9e842b96a4
Alihussnain076 <alishergehlan1@gmail.com>
2026-07-23 16:55:27 +0500

    feat(backend): integrate Pino logger

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./src/config/db');
+const authRoutes = require('./src/routes/authRoutes');
+const noteRoutes = require('./src/routes/noteRoutes');
+const errorHandler = require('./src/middleware/errorHandler');
+const logger = require('./src/config/logger');
+const pinoHttp = require('pino-http');
 
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(pinoHttp({ logger }));
 
-// Test route
+app.use('/api/auth', authRoutes);
+app.use('/api/notes', noteRoutes);
 app.get('/api/health', (req, res) => {
+  logger.info('Health check endpoint called');
   res.status(200).json({ status: 'OK', message: 'Server is running' });
 });
 
-// Start server
+app.use(errorHandler);
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  logger.info(`Server is running on port ${PORT}`);
 });
